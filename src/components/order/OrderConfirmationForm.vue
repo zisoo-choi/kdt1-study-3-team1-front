298f139bba435728be99612f981e96d70984b951
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-05-25 17:53:53 +0900

    fix: [유경우] accountId -> loginuserId로 [FPPx]

--- src/components/order/OrderConfirmationForm.vue
+++ src/components/order/OrderConfirmationForm.vue
@@ -27,7 +27,7 @@
 export default {
     data () {
         return {
-            accountId: localStorage.getItem('accountId')
+            accountId: localStorage.getItem('loginuserId')
         }
     },
     props: {
