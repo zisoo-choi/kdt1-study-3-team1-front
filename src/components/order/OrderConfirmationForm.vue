ce9a7736898ad0c79da1406ec63558971936a206
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-05-25 16:43:44 +0900

    feat:[유경우] 장바구니 페이지나 상품 상세 페이지에서 구매 버튼을 누르면 구매 됩니다[FPP-7]

---
+++ src/components/order/OrderConfirmationForm.vue
@@ -0,0 +1,49 @@
+<template lang="">
+    <form>
+            <div> 
+            <b>상품 번호</b>
+            {{ product.productId }}
+        </div>
+        <div> 
+            <b>상품 명</b>
+            {{ product.productName }}
+        </div>
+        <div> 
+            <b>상품 가격</b>
+            {{ product.productPrice }}
+        </div>
+        <v-btn @click="onSubmit">구매</v-btn> 
+        <button>
+            <router-link :to="{ name: 'home' }">
+                돌아가기
+            </router-link>            
+        </button>
+    </form>
+</template>
+
+<script>
+
+
+export default {
+    data () {
+        return {
+            userToken: localStorage.getItem('loginUserToken')
+        }
+    },
+    props: {
+        product: {
+            type: Object,
+            required: true,
+        }
+    },
+    methods: {
+        onSubmit () {
+            const { productName, productPrice, userToken } = this
+            this.$emit('submit', { productName, productPrice, userToken })
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
