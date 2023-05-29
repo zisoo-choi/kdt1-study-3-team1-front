5657f6b7f76710edf5f6a72558a11fa3045befbf
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-05-30 01:57:43 +0900

    feat: [유경우] env설정 및 도커 컴포즈 설정 [FPPx]

--- src/utility/axiosInst.js
+++ src/utility/axiosInst.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import env from "@/env";
 
 const axiosInst = axios.create({
-    baseURL: 'http://localhost:7777',
-timeout: 2500
+    baseURL: env.api.MAIN_API_URL,
+    timeout: 2500
 })
 
 export default axiosInst
